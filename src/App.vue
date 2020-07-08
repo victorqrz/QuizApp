1caacc9e0b55198df3ae0eb249a96dd7c652c083
Victor <victoorqrz@gmail.com>
2020-07-08 20:56:59 -0300

    bootstrap configs done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app">
+
     <Header />
-    <QuestionBox />
+
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox />
+        </b-col>
+      </b-row>
+    </b-container>
+  
   </div>
 </template>
 
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="question-box-container">
-    <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
-      <p>For more information visit website</p>
-      <b-button variant="primary" href="#">More Info</b-button>
+    <b-jumbotron>
+
+      <template v-slot:lead>
+        Questions !
+      </template>
+
+      <hr class="my-4">
+
+      <p>
+        List of answers !!
+      </p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+
     </b-jumbotron>
   </div>
 </template>
